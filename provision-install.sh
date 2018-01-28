84439e4164b62c11fc75937aa893da01a8fa92a5
Jason Pham <me@jasonpham.ca>
2018-01-27 23:52:50 -0500

    Auto confirm installation

--- provision-install.sh
+++ provision-install.sh
@@ -1,10 +1,10 @@
-sudo apt-get update && sudo apt-get upgrade
-sudo apt-get install python-pip
+sudo apt-get update && sudo apt-get -y upgrade
+sudo apt-get -y install python-pip
 sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
 sudo apt-get update
-sudo apt-get install gcc-5 g++-5
+sudo apt-get -y install gcc-5 g++-5
 sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-5 60 --slave /usr/bin/g++ g++ /usr/bin/g++-5
 sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-5 40 --slave /usr/bin/g++ g++ /usr/bin/g++-5
 sudo update-alternatives --config gcc
-sudo apt-get install python-pkg-resources libboost-regex-dev debhelper libgmp3-dev automake autoconf texlive texlive-latex-recommended texlive-fonts-recommended texlive-latex-extra texlive-lang-cyrillic texlive-lang-english tidy ghostscript
+sudo apt-get -y install python-pkg-resources libboost-regex-dev debhelper libgmp3-dev automake autoconf texlive texlive-latex-recommended texlive-fonts-recommended texlive-latex-extra texlive-lang-cyrillic texlive-lang-english tidy ghostscript
 sudo pip2 install pytest pyyaml plastex
