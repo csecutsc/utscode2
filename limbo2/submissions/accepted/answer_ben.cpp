240c17303a498145d4cc490770b7b7a9bc35cc7c
Alex Li <me@alexli.ca>
2018-09-10 21:41:37 -0400

    Limbo answers

---
+++ limbo2/submissions/accepted/answer_ben.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+long long hibit(long long x) {
+  if (x == 0) {
+    return 0;
+  }
+  x >>= 1;
+  long long ret = 1;
+  while (x > 0) {
+    x >>= 1;
+    ret <<= 1;
+  }
+  return ret;
+}
+
+int main() {
+  long long r, c;
+  cin >> r >> c;
+
+  long long hr = hibit(r);
+  long long hc = hibit(c);
+
+  if (hr >= hc) {
+    cout << 2*hr*hr + (r - hr)*hr + c << endl;
+  } else {
+    cout << hc*hc + (c - hc)*hc + r << endl;
+  }
+  return 0;
+}
