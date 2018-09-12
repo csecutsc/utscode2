523961a477ce592d99bd703414a272a8c3b29811
Jason Pham <me@jasonpham.ca>
2018-09-11 20:40:27 -0400

    Update limbo1 data

--- limbo1/input_format_validators/validate.py
+++ limbo1/input_format_validators/validate.py
@@ -4,11 +4,16 @@ import re
 import sys
 
 integer = "(0|-?[1-9]\d*)"
+single_integer = re.compile(integer + '\n')
 two_int_first_line = re.compile(integer + " " + integer + "\n")
 
 line = stdin.readline()
-assert two_int_first_line.match(line)
-r, d = map(int, line.split())
-assert 0 <= r <= 2**31-1 and 0 <= d <= 2**31-1
+assert single_integer.match(line)
+
+for _ in range(int(line)):
+    n_line = stdin.readline()
+    assert two_int_first_line.match(n_line)
+    r, d = map(int, n_line.split())
+    assert 1 <= r <= 10**9 and 1 <= d <= 10**9
 
 sys.exit(42)
