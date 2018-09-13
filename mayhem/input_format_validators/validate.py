08235cbba23574a383711f392c29f1b819108648
Alex Li <alextrovert@gmail.com>
2018-09-13 16:05:59 -0400

    Mayhem validator

--- mayhem/input_format_validators/validate.py
+++ mayhem/input_format_validators/validate.py
@@ -1,4 +1,26 @@
 #!/usr/bin/env python3
+from sys import stdin
+import re
 import sys
 
+integer = "(0|-?[1-9]\\d*)"
+
+line = sys.stdin.readline()
+assert re.match(integer + " " + integer + "\n", line), \
+    "'{}' is not a pair of integers".format(line)
+
+R, C = map(int, line.split())
+assert 1 <= R <= 2000, "R = {} is not in [1, 2000]".format(R)
+assert 1 <= C <= 2000, "C = {} is not in [1, 2000]".format(C)
+
+grid = []
+for r in range(R):
+    line = stdin.readline()
+    assert re.match('(x|\\.){{{}}}'.format(C) + "\n", line), \
+        "Line must be consist of {} '#'s or '.'s. Got `{}`.".format(C, line)
+    grid.append(line.strip())
+
+assert len(sys.stdin.readline()) == 0, "Extra lines detected after input."
+
+# Nothing to report.
 sys.exit(42)
