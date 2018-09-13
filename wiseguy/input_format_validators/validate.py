fc7e0f4d1bbeaafe2d53d6e55721f88ad4d3f182
Alex Li <alextrovert@gmail.com>
2018-09-13 16:30:59 -0400

    Update thanos and wiseguy

--- wiseguy/input_format_validators/validate.py
+++ wiseguy/input_format_validators/validate.py
@@ -1,4 +1,26 @@
 #!/usr/bin/env python3
+import re
 import sys
 
+integer = "(0|-?[1-9]\\d*)"
+MAX = 500
+
+line = sys.stdin.readline()
+assert re.match(integer + "\n", line), "'{}' is not an int".format(line)
+T = int(line)
+assert 1 <= T <= 500, "T = {} is not in [1, 500]".format(n, MAX)
+
+cases = 0
+while True:
+    line = sys.stdin.readline()
+    if len(line) == 0:
+        break
+    assert re.match(integer + "\n", line), "'{}' is not an int".format(line)
+    N = int(line)
+    assert 1 <= N <= MAX, "{} is not in [0, {}]".format(n, MAX)
+    cases += 1
+
+assert cases == T, "number of cases ({}) not equal to T ({})".format(cases, T)
+
+# Nothing to report.
 sys.exit(42)
