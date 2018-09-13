6c0afc0d186b9828ec83e91e7d240b61b1f80c47
Alex Li <alextrovert@gmail.com>
2018-09-13 15:50:21 -0400

    Update limbo2

--- limbo2/input_format_validators/validate.py
+++ limbo2/input_format_validators/validate.py
@@ -1,4 +1,24 @@
 #!/usr/bin/env python3
+from sys import stdin
+import re
 import sys
+
+integer = "(0|-?[1-9]\\d*)"
+
+line = stdin.readline()
+assert re.match(integer + "\n", line), \
+    "Expected a single integer, got {}".format(line)
+T = int(line)
+assert 1 <= T <= 100, "T = {} is not in [1, 50]".format(n, MAX)
+
+for _ in range(int(line)):
+    line = stdin.readline()
+    assert re.match(integer + " " + integer + "\n", line), \
+        "Expected a pair of integers, got {}.".format(line)
+    R, D = map(int, line.split())
+    assert 1 <= R <= 10**9, "R = {} must be in [1, 10^9]".format(R)
+    assert 1 <= D <= 10**9, "D = {} must be in [1, 10^9]".format(D)
+
+assert len(sys.stdin.readline()) == 0, "Extra lines detected after input."
 
 sys.exit(42)
