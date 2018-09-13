55c07d3c976196dade24af5bb1c2fa9790c87eae
Alex Li <alextrovert@gmail.com>
2018-09-13 20:45:59 +0000

    Add verifier shell script and regenerate all PDFs

---
+++ verify.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+rm -f beautifulprimes.pdf
+rm -f chocolates.pdf
+rm -f enigma.pdf
+rm -f limbo1.pdf
+rm -f limbo2.pdf
+rm -f mayhem.pdf
+rm -f piperotation.pdf
+rm -f polarexpress.pdf
+rm -f thanos.pdf
+rm -f wiseguy.pdf
+
+verifyproblem beautifulprimes
+verifyproblem chocolates
+verifyproblem enigma
+verifyproblem limbo1
+verifyproblem limbo2
+verifyproblem mayhem
+verifyproblem piperotation
+verifyproblem polarexpress
+verifyproblem thanos
+verifyproblem wiseguy
+
+problem2pdf beautifulprimes
+problem2pdf chocolates
+problem2pdf enigma
+problem2pdf limbo1
+problem2pdf limbo2
+problem2pdf mayhem
+problem2pdf piperotation
+problem2pdf polarexpress
+problem2pdf thanos
+problem2pdf wiseguy
