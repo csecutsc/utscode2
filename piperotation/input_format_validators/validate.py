01c38fb527fd8eb9540b44b2675d3bf50d45a723
William Granados <me@wgma00.me>
2018-09-16 18:52:02 -0400

    changed variable name for rows and columns in piperotation problem

--- piperotation/input_format_validators/validate.py
+++ piperotation/input_format_validators/validate.py
@@ -9,15 +9,15 @@ line = sys.stdin.readline()
 assert re.match(integer + " " + integer + "\n", line), \
     "'{}' is not a pair of integers".format(line)
 
-R, C = map(int, line.split())
-assert 1 <= R <= 100, "R = {} is not in [1, 100]".format(R)
-assert 1 <= C <= 100, "C = {} is not in [1, 100]".format(C)
+N, M = map(int, line.split())
+assert 1 <= N <= 100, "N = {} is not in [1, 100]".format(N)
+assert 1 <= M <= 100, "M = {} is not in [1, 100]".format(M)
 
 grid = []
-for r in range(R):
+for r in range(N):
     line = stdin.readline()
-    assert re.match('[ABCD]{{{}}}'.format(C) + "\n", line), \
-        "Line must be consist of {} chars from A-D. Got `{}`.".format(C, line)
+    assert re.match('[ABCD]{{{}}}'.format(M) + "\n", line), \
+        "Line must be consist of {} chars from A-D. Got `{}`.".format(M, line)
     grid.append(line.strip())
 
 assert len(sys.stdin.readline()) == 0, "Extra lines detected after input."
