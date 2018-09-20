e424139e1e6e1dac987fb207b0e9b7d275063578
Alex Li <alextrovert@gmail.com>
2018-09-19 20:09:17 -0400

    Commit some WA solutions to beautifulprimes

---
+++ beautifulprimes/submissions/wrong_answer/bigoutput.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python3
+
+for i in range(100000000):
+    print('1', end='')
---
+++ beautifulprimes/submissions/wrong_answer/negatives.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+T = int(input())
+for _ in range(T):
+    N = int(input())
+    print(' '.join(['-1']*N))
