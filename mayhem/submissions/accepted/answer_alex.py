011608e028e1770061ae3796fff234c144fd9607
Alex Li <alextrovert@gmail.com>
2018-09-19 21:31:30 -0400

    Add mayhem Python solution

--- mayhem/submissions/accepted/answer_alex.py
+++ mayhem/submissions/accepted/answer_alex.py
@@ -5,43 +5,87 @@ from collections import deque
 R, C = map(int, input().split())
 G = [list(input().strip()) for _ in range(R)]
 
-adj = [[[] for c in range(C)] for r in range(R)]
+total = 0
+components = 0
+
+# Initialize pointers.
+lcol = [[-1 for c in range(C)] for r in range(R)]
+rcol = [[-1 for c in range(C)] for r in range(R)]
+urow = [[-1 for c in range(C)] for r in range(R)]
+drow = [[-1 for c in range(C)] for r in range(R)]
 
 for i in range(R):
-    p = -1
+    curr = -1
     for j in range(C):
         if G[i][j] == 'x':
-            if p < 0:
-                p = j
-            else:
-                adj[i][j].append((i, p))
-                adj[i][p].append((i, j))
+            lcol[i][j] = curr
+            curr = j
+    curr = -1
+    for j in range(C - 1, -1, -1):
+        if G[i][j] == 'x':
+            rcol[i][j] = curr
+            curr = j
 
 for j in range(C):
-    p = -1
+    curr = -1
     for i in range(R):
         if G[i][j] == 'x':
-            if p < 0:
-                p = i
-            else:
-                adj[i][j].append((p, j))
-                adj[p][j].append((i, j))
+            urow[i][j] = curr
+            curr = i
+    curr = -1
+    for i in range(R - 1, -1, -1):
+        if G[i][j] == 'x':
+            drow[i][j] = curr
+            curr = i
 
-ans = 0
+
+def update_pointers(r, c):
+    if lcol[r][c] != -1:
+        if rcol[r][c] != -1:
+            rcol[r][lcol[r][c]] = rcol[r][c];
+            lcol[r][rcol[r][c]] = lcol[r][c];
+        else:
+            rcol[r][lcol[r][c]] = -1;
+
+    if urow[r][c] != -1:
+        if drow[r][c] != -1:
+            drow[urow[r][c]][c] = drow[r][c];
+            urow[drow[r][c]][c] = urow[r][c];
+        else:
+            drow[urow[r][c]][c] = -1;
+
+    if rcol[r][c] != -1:
+        if lcol[r][c] != -1:
+            lcol[r][rcol[r][c]] = lcol[r][c];
+            rcol[r][lcol[r][c]] = rcol[r][c];
+        else:
+            lcol[r][rcol[r][c]] = -1;
+
+    if drow[r][c] != -1:
+        if urow[r][c] != -1:
+            urow[drow[r][c]][c] = urow[r][c];
+            drow[urow[r][c]][c] = drow[r][c];
+        else:
+            urow[drow[r][c]][c] = -1;
+
+
 q = deque()
 
 for i in range(R):
     for j in range(C):
         if G[i][j] == 'x':
-            ans -= 1
+            components += 1
             q.append((i, j))
-            G[i][j] = '.'
             while q:
                 r, c = q.popleft()
-                ans += 1
-                for r2, c2 in adj[r][c]:
-                    if G[r2][c2] == 'x':
-                        q.append((r2, c2))
-                        G[r2][c2] = '.'
+                if G[r][c] == '.':
+                    continue
+                total += 1
+                G[r][c] = '.'
+                update_pointers(r, c)
+                if lcol[r][c] != -1: q.append((r, lcol[r][c]))
+                if urow[r][c] != -1: q.append((urow[r][c], c))
+                if rcol[r][c] != -1: q.append((r, rcol[r][c]))
+                if drow[r][c] != -1: q.append((drow[r][c], c))
 
-print(ans)
+print(total - components)
